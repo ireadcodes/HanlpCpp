0c04af50d8f8803e187790341ec0f2dbe00b225e
Lazybones3 <caorui452@163.com>
2024-09-04 14:37:03 +0800

    DemoHightlightKeyword

--- test/other/DemoHighlightKeyword.cpp
+++ test/other/DemoHighlightKeyword.cpp
@@ -3,14 +3,14 @@
 #include <string>
 #include <vector>
 
-std::wstring highlight(const std::wstring& text, std::vector<std::wstring> keyword_list) {
-    std::wstringstream result;
+std::string highlight(const std::string& text, std::vector<std::string> keyword_list) {
+    std::stringstream result;
     int index = 0;
     while (index < text.size()) {
         bool found = false;
-        for (std::wstring keyword : keyword_list) {
+        for (std::string keyword : keyword_list) {
             if (text.compare(index, keyword.size(), keyword) == 0) {
-                result << L"<span style='color:red;'>" << keyword << L"</span>";
+                result << "<span style='color:red;'>" << keyword << "</span>";
                 index += keyword.size();
                 found = true;
                 break;
@@ -25,11 +25,9 @@ std::wstring highlight(const std::wstring& text, std::vector<std::wstring> keywo
 }
 
 int main() {
-    std::locale::global(std::locale(""));
-    
-    std::wstring text = L"天气预报说明天出太阳";
-    std::vector<std::wstring> keyword_list = { L"明天", L"太阳" };
-    std::wstring result = highlight(text, keyword_list);
-    std::wcout << result << std::endl;
+    std::string text = "天气预报说明天出太阳";
+    std::vector<std::string> keyword_list = { "明天", "太阳" };
+    std::string result = highlight(text, keyword_list);
+    std::cout << result << std::endl;
     return 0;
 }
